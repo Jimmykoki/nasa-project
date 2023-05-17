28a9d69fb12ff5bea80f13c30635e762cfe1ff7b
Jimmykoki <jianlil2@uci.edu>
2023-05-17 10:26:00 -0700

    feat(server): introduced dotenv configue to serve

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const app = require('./app');
 
+require('dotenv').config();
+
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
@@ -10,6 +14,8 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
+
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
